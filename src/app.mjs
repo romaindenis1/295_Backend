d2e7e3032e6504e9289f35bcbfb7897e649e18dc
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-01-15 10:46:17 +0100

    Commit Add Product
    You can now add products with a curl

--- src/app.mjs
+++ src/app.mjs
@@ -2,14 +2,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 //Default
 app.get("/", (req, res) => {
-res.send("API REST of self service machine !");
+  res.send("API REST of self service machine !");
 });
 
 //redirige /api/ vers localhost 3000
 app.get("/api/", (req, res) => {
-res.redirect(`http://localhost:${port}/`);
+  res.redirect(`http://localhost:${port}/`);
 });
 
 //prend la methode de products
@@ -18,5 +20,5 @@ import { productsRouter } from "./routes/products.mjs";
 //dire que ca marche
 app.use("/api/products", productsRouter);
 app.listen(port, () => {
-console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,13 +2,41 @@
 import express from "express";
 import { products } from "../db/mock-product.mjs";
 import { success } from "./helper.mjs";
+import { getUniqueId } from "./helper.mjs";
 
 const productsRouter = express();
 
 //apelle fonction pour return en json
 productsRouter.get("/", (req, res) => {
-const message = "La liste des produits a bien été récupérée.";
-res.json(success(message, products));
+  const message = "La liste des produits a bien été récupérée.";
+  res.json(success(message, products));
+});
+
+// Recupere une entree dependant sur l'id
+// ex: http://localhost:3000/api/products/3 donne les infos de ID 3
+productsRouter.get("/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((product) => product.id == productId);
+  const message = `Le produit dont l'id vaut ${productId} a bien été récupéré.`;
+  res.json(success(message, product));
+});
+
+productsRouter.post("/", (req, res) => {
+  // Création d'un nouvel id du produit
+  // Dans les prochains versions, c'est MySQL qui gérera cela pour nous (identifiant auto_increment)
+  const id = getUniqueId(products);
+
+  // Création d'un objet avec les nouvelles informations du produits
+  const createdProduct = { ...req.body, ...{ id: id, created: new Date() } };
+
+  // Ajout du nouveau produit dans le tableau
+  products.push(createdProduct);
+
+  // Définir un message pour le consommateur de l'API REST
+  const message = `Le produit ${createdProduct.name} a bien été créé !`;
+
+  // Retourner la réponse HTTP en json avec le msg et le produit créé
+  res.json(success(message, createdProduct));
 });
 
 //exporter
